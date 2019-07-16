80e5b169f96400db7175a5592b5e0238b44919dd
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-07-16 16:01:01 +0100

    Fix indentation & read html file

--- legacy/projects/state-mach-regex/extractlinks/extract_links.py
+++ legacy/projects/state-mach-regex/extractlinks/extract_links.py
@@ -2,16 +2,19 @@ import re # module for processing regular expressions https://docs.python.org/3/
 import sys
 import csv
 if __name__ == '__main__':
-  # Exit if command line args entered incorrectly
-  if len(sys.argv) != 2:
-    print("usage: extract_links.py [input_file]")
-    sys.exit(0)
+    # Exit if command line args entered incorrectly
+    if len(sys.argv) != 2:
+        print("usage: extract_links.py [input_file]")
+        sys.exit(0)
 
 # Filename is 2nd command line arg
 filename = sys.argv[1]
 
 # TODO Read HTML file
 
+with open(filename) as file:
+    filedata = file.read()
+    file.close()
 
 # TODO Set up regex
 
