58e676c622c3d11d9cef6bd2d4fe4d27b12b3f7c
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-07-16 16:28:16 +0100

    Read links from answers

--- legacy/projects/state-mach-regex/extractlinks/extract_links.py
+++ legacy/projects/state-mach-regex/extractlinks/extract_links.py
@@ -27,18 +27,26 @@ matches = re.findall(regex, filedata)
 
 
 # Check matches, print results
-# TODO Read in links from answers.txt (hint...this is a CSV file), 
+
+# Read in links from answers.txt (hint...this is a CSV file),
 # save in list called 'answer_data'
 
+with open('answers.txt', newline="") as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    for row in reader:
+        answer_data = row
+
+    for answer in answer_data:
+        print(answer)
 
 # Compare answers with matches found using regex, print out any mismatches
 # UNCOMMENT BELOW WHEN READY TO CHECK IF YOUR REGEX IS FINDING ALL THE LINKS
-# result = "All links matched!"
-# if len( matches ) != len( answer_data ):
-#   result = "Your regex found %i matches. There should be %i matches" %(len( matches ), len( answer_data ) )
-# else:
-#   for i in range( len(answer_data) ):
-#     if( matches[i] != answer_data[i] ):
-#       result = "Mismatched link. Got %s but expected %s" % ( matches[i], answer_data[i] )
-#       break
-# print( result )
+result = "All links matched!"
+if len(matches) != len(answer_data):
+  result = "Your regex found %i matches. There should be %i matches" %(len(matches), len(answer_data))
+else:
+  for i in range( len(answer_data) ):
+    if(matches[i] != answer_data[i]):
+      result = "Mismatched link. Got %s but expected %s" % (matches[i], answer_data[i])
+      break
+print(result)
